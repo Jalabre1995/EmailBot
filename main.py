8ce36acee5e5a9a7adfff3fba03167ef601a9876
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2021-01-26 16:00:33 -0500

    Adding parameters to the send email function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import speech_recognition as sr
 #Import speech Recognition using PyAudio 
 #Import python text to speech
 import pyttsx3
+from email.message import EmailMessage
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 #Create a function called talk and its going to have the parameter text and it eill wait for the text and await foir it.
@@ -27,22 +28,41 @@ def get_Info():
     except:
             pass
         #Put the send email into a function . 
-def send_email():
+def send_email(receiver, subject, message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     #tellingthe server trust transport security
     server.starttls()
     #Telling the server who we are
     server.login('Jdog1500xx@gmail.com', 'Josh1995@10')
+    email = EmailMessage()
+    email['From'] = 'jdog1500xx@gmail.com'
+    email ['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
     #Sending the email, there is going to be three parameters int his email 1: the person sending the email, 2: who you are sending it to, 3: the nessage.
-    server.sendmail('Jdog1500xx@gmail.com', 
-                    'joshuaalabre@gmail.com',
-                    'Hey wanna play some poker tomorrow evening?'
+    server.sendmail(email)
 
-)
+
+
+email_list = {
+    
+
+
+}
 
 def get_email_info():
     talk('To whom you want to send the email to')
     name = get_Info()
+    reciever = email_list[name]
+    print(reciever)
+    talk('What is the subject of the email?')
+    subject = get_info()
+    print(subject)
+    talk('Tell me the text in your email')
+    message = get_info()
+    print(message)
+
+    send_email(reciever,subject,message )
 
 
 get_email_info()
