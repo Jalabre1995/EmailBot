e3758ae57a9b2e5960a1816d93626af42d45393e
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2021-01-26 14:01:57 -0500

    send a message to another email address.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import smtplib
+import smtplib 
 #Importing the smtplibb  library 
-server = smtplib.SMP('smtp.gmail.com', 587)
+server = smtplib.SMTP('smtp.gmail.com', 587)
 #tellingthe server trust transport security
-server.strarttls()
+server.starttls()
 #Telling the server who we are
-server.login('fresh.grandpa21@gmail.com', 'theoldies1954')
+server.login('Jdog1500xx@gmail.com', 'Josh1995@10')
 #Sending the email, there is going to be three parameters int his email 1: the person sending the email, 2: who you are sending it to, 3: the nessage.
-server.sendmail('fresh.grandpa21@gmail.com', 
-                'finestmanalive21@gmnail.com',
+server.sendmail('Jdog1500xx@gmail.com', 
+                'joshuaalabre@gmail.com',
                 'Hey wanna play some poker tomorrow evening?'
 
 )
